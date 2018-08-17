ef4fcd4fb2c02a5bc8e29e390235e1f845ea67e6
xiongben <wangdongkf@163.com>
2018-08-17 17:11:09 +0800

    至此mq的收发消息算是Ok

--- src/main/java/com/newframe/services/rocketmq/impl/TestRocketMQServiceImpl.java
+++ src/main/java/com/newframe/services/rocketmq/impl/TestRocketMQServiceImpl.java
@@ -1,12 +1,15 @@
 package com.newframe.services.rocketmq.impl;
 
 import com.newframe.dto.OperationResult;
+import com.newframe.dto.rocketmq.OrderPaidEvent;
 import com.newframe.enums.BizErrorCode;
 import com.newframe.services.rocketmq.TestRocketMQService;
 import org.apache.rocketmq.spring.starter.core.RocketMQTemplate;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 
 /**
  * @author:wangdong
@@ -27,7 +30,9 @@ public class TestRocketMQServiceImpl implements TestRocketMQService{
     @Override
     public OperationResult<Boolean> sendMessage() {
 
-        rocketMQTemplate.convertAndSend("string-topic","Hello World");
+        rocketMQTemplate.convertAndSend("test-topic-1", "Hello, World!");
+        rocketMQTemplate.send("test-topic-1", MessageBuilder.withPayload("Hello, World! I'm from spring message").build());
+        rocketMQTemplate.convertAndSend("test-topic-2", new OrderPaidEvent("T_001", new BigDecimal("88.00")));
 
         return new OperationResult<>(true);
     }
