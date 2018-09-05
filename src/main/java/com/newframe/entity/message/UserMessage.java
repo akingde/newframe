a7baa171d0ef077d7edac4dc16e75da1a5164b0d
xiongben <wangdongkf@163.com>
2018-09-05 10:10:05 +0800

    修复查询消息

---
+++ src/main/java/com/newframe/entity/message/UserMessage.java
@@ -0,0 +1,58 @@
+package com.newframe.entity.message;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * <p>
+ * 资金方账户表
+ * </p>
+ *
+ * @author zww
+ * @since 2018-08-29
+ */
+@Data
+@Entity
+public class UserMessage {
+    /**
+     * id
+     */
+    @Id
+    private Long id;
+
+    /**
+     * 用户的Uid
+     */
+    private Long uid;
+    /**
+     * 平台自己的订单ID
+     */
+    private Long orderId;
+    /**
+     * 关联订单的ID
+     */
+    private Long associatedOrderId;
+    /**
+     * 消息的标题
+     */
+    private String messTitle;
+    /**
+     * 消息的类型：1:融资类消息，2:租机类消息，3:发货申请类的消息
+     */
+    private Integer messType;
+    /**
+     * 消息的内容
+     */
+    private String messContent;
+    /**
+     * ctime
+     */
+    private Integer ctime;
+    /**
+     * utime
+     */
+    private Integer utime;
+}
--- src/main/java/com/newframe/services/common/CommonService.java
+++ src/main/java/com/newframe/services/common/CommonService.java
@@ -2,6 +2,7 @@ package com.newframe.services.common;
 
 import com.newframe.dto.OperationResult;
 import com.newframe.dto.common.ExpressInfo;
+import com.newframe.dto.message.UserMessageInfo;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,4 +19,13 @@ public interface CommonService {
      * @return
      */
     OperationResult<ExpressInfo> getExpressMessage(String expCode, String expNo);
+
+    /**
+     * 首页查询用户的消息列表
+     * @param uid
+     * @param currentPage
+     * @param pageSize
+     * @return
+     */
+    OperationResult<UserMessageInfo> listUserMessage(Long uid, Integer currentPage, Integer pageSize);
 }
