1980d7a27aef6d99bdad0a5f3384428d86deed3a
WBin <wangb@disanbo.com>
2018-09-06 16:03:04 +0800

    添加了对身份证号的校验

--- src/main/java/com/newframe/services/user/roleimpl/FirstRentMerchantServiceImpl.java
+++ src/main/java/com/newframe/services/user/roleimpl/FirstRentMerchantServiceImpl.java
@@ -281,6 +281,9 @@ public class FirstRentMerchantServiceImpl implements RoleService {
         if (StringUtils.isEmpty(rentMerchantApplyDTO.getConsigneeAddress())){
             return new OperationResult(RequestResultEnum.ADDRESS_ERROR, false);
         }
+        if(IdNumberUtils.isValidatedAllIdcard(rentMerchantApplyDTO.getLegalEntityIdNumber())){
+            return new OperationResult(RequestResultEnum.ID_NUMBER_ERROR, false);
+        }
         return new OperationResult(true);
     }
 
