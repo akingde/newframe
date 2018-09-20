2be76b2c0115f6cfa51767777fb245a30d9edf69
WBin <wangb@disanbo.com>
2018-09-20 11:20:11 +0800

    提交了平台银行的信息

--- src/main/java/com/newframe/enums/PlatformBank.java
+++ src/main/java/com/newframe/enums/PlatformBank.java
@@ -9,8 +9,8 @@ import lombok.Data;
 @Data
 public class PlatformBank {
 
-    private final String platformName = "戴涛";
+    private final String platformName = "杭州多链科技有限公司";
     private final String platformBankNumber = "33050110451700000060";
-    private final String platformBankName = "建设银行";
-    private final String platformBankDetailedName = "中国建设银行杭州文晖支行";
+    private final String platformBankName = "中国建设银行";
+    private final String platformBankDetailedName = "建行杭州城西支行";
 }
