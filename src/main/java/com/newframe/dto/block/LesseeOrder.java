55abb397fa367bddb599205d3766dc0d7143f5ac
WBin <wangb@disanbo.com>
2018-09-21 14:45:30 +0800

    提交了合约的接口参数注释

--- src/main/java/com/newframe/dto/block/LesseeOrder.java
+++ src/main/java/com/newframe/dto/block/LesseeOrder.java
@@ -3,12 +3,30 @@ package com.newframe.dto.block;
 import lombok.Data;
 
 /**
+ * 第三方订单信息
  * @author WangBin
  */
 @Data
 public class LesseeOrder extends ProdDetail {
+
+    /**
+     * 第三方订单id
+     */
     private String orderId;
+    /**
+     * 第三方订单推过来的时间
+     */
     private Integer orderTime;
+    /**
+     * 租客手机号
+     */
     private String phoneNum;
+    /**
+     * 租客姓名
+     */
     private String realName;
+    /**
+     * 租客手机号
+     */
+    private String idNum;
 }
